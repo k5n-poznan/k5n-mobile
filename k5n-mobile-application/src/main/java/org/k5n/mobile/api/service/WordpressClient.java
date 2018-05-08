815cbdec120f42aa64168deaac950b9bf39044bd
Waldemar Kłaczyński <wklaczynski@sabaservice.pl>
2018-05-09 00:14:53 +0200

    Create Mobile Application

--- k5n-mobile-application/src/main/java/org/k5n/mobile/api/service/WordpressClient.java
+++ k5n-mobile-application/src/main/java/org/k5n/mobile/api/service/WordpressClient.java
@@ -5,10 +5,42 @@
  */
 package org.k5n.mobile.api.service;
 
+import java.io.IOException;
+import org.k5n.mobile.api.MobilePlatform;
+import org.k5n.mobile.api.Properties;
+import org.k5n.mobile.api.SettingService;
+import org.k5n.mobile.api.auth.Credentials;
+
 /**
  *
  * @author Waldemar Kłaczyński
  */
 public class WordpressClient {
-    
+
+    private String endpointURL;
+
+    public void test() {
+        
+    }
+
+    private void init(Credentials credentials) {
+        try {
+            MobilePlatform mp = MobilePlatform.getIstance();
+            SettingService setting = mp.getSettingService();
+
+            endpointURL = setting.retrieve(Properties.ENDPOINT_URL);
+            if (endpointURL == null) {
+                throw new IOException("Nie ustawiono adresu banku w ustawieniach.");
+            }
+
+            if (!endpointURL.endsWith("/")) {
+                endpointURL += "/";
+            }
+            endpointURL += "index.php/wp-json/wp-msg/v1";
+
+        } catch (Throwable th) {
+            throw new RuntimeException(th);
+        }
+    }
+
 }
